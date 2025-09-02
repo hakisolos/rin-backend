cb4c2dfe38328bf0cd9d129c0422d1ee2fb17515
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-09-02 19:54:43 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
 import { Hono } from "hono"
+import { cors } from "hono/cors"
 import rin from "./src/routes/rin"
 
 const app = new Hono()
+
+app.use("/*", cors())
+
+
 app.route("/rin", rin)
-
-
-
-
-
 
 Bun.serve({
   fetch: app.fetch,
   port: 8080,
 })
 
-console.log(`app running on port 8080`)
+console.log("app running on port 8080")
